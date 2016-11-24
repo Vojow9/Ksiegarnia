84ffae88b3e65464e2e56b6ae3de2ed0bce41651
kubapok <jakubpokrywka@gmail.com>
2016-11-24 11:09:51 +0100

    refaktoryzacja

--- serwer/models/books.py
+++ serwer/models/books.py
@@ -1,5 +1,6 @@
 import json, pymongo
 from pymongo import MongoClient
+from bson import ObjectId
 from bson.json_util import dumps
 
 def connect_to_database(host='localhost', port=27017):
@@ -14,7 +15,5 @@ class Books():
     books = db.books
     def getAllBooks():
         return dumps(Books.books.find())
-    def getAllIdAndTittles():
-        return {b['id']:b['name'] for b in books_example}
     def getBookById(id):
-        return dumps(Books.books.find_one({"id":str(id)}))
+        return dumps(Books.books.find_one({'_id' :ObjectId(id)}))
