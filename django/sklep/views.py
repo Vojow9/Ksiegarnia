879c11db32756f8090f2434446546ebf4ddc50eb
Megii <magdalena.wojciechowska@poczta.onet.pl>
2016-12-04 20:09:27 +0100

    zakupy

--- django/sklep/views.py
+++ django/sklep/views.py
@@ -11,3 +11,5 @@ def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'sklep/book_detail.html', {'book': book})
 
+def shop_detail(request):
+	return render(request, 'sklep/shop_detail.html')
