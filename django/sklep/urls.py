aaddadd8b22f2ea6312edc1c310db2fa03bff356
Megii <magdalena.wojciechowska@poczta.onet.pl>
2016-12-04 17:45:11 +0100

    chwilowe usuniecie

--- django/sklep/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.book_list, name='book_list'),
-    url(r'^book/(?P<pk>[0-9]+)/$', views.book_detail, name='book_detail'),
-]
