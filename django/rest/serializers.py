6468f4988a09ad3b90156b78e4cf4c0d803b8719
Vojow9 <vojow9@gmail.com>
2016-12-06 16:31:22 +0100

    rest

--- django/rest/serializers.py
+++ django/rest/serializers.py
@@ -4,4 +4,4 @@ from sklep.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ('id','author','title','description','tableOfContents','price','ebook','paper','graphics','published_date')
+        fields = ('id','author','title','description','tableOfContents','price','graphics','published_date')
