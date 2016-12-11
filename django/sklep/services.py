4ced836936932d9b62edd2d8618fb4f10e6a5474
kubapok <jakubpokrywka@gmail.com>
2016-12-11 14:23:50 +0100

    works with rest api and id instead of oid

--- django/sklep/services.py
+++ django/sklep/services.py
@@ -5,3 +5,9 @@ def getBooksList():
     books = requests.get('http://localhost:8080/books').json()
     print(books)
     return books
+
+
+def getBookById(id):
+    books = requests.get('http://localhost:8080/books/' + id).json()
+    print(books)
+    return books
