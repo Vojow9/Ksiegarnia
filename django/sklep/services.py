505292967f574ff9afc2e31c6c959d2ee833f86b
kubapok <jakubpokrywka@gmail.com>
2016-12-11 14:43:52 +0100

    django works with rest api- kubas part- include only list of books

--- django/sklep/services.py
+++ django/sklep/services.py
@@ -3,11 +3,13 @@ import requests
 
 def getBooksList():
     books = requests.get('http://localhost:8080/books').json()
-    print(books)
     return books
 
 
 def getBookById(id):
     books = requests.get('http://localhost:8080/books/' + id).json()
-    print(books)
     return books
+
+
+if __name__ == '__main__':
+    print(getBooksList())
--- django/sklep/views.py
+++ django/sklep/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .services import getBooksList
+from .services import getBooksList, getBookById
 from django.shortcuts import render, get_object_or_404
 
 def book_list(request):
@@ -8,7 +8,7 @@ def book_list(request):
     return render(request, 'sklep/book_list.html', {'books':books})
 
 def book_detail(request, pk):
-    book = get_object_or_404(Book, pk=pk)
+    book = getBookById(pk)
     return render(request, 'sklep/book_detail.html', {'book': book})
 
 def shop_detail(request):
