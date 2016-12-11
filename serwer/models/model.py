2a379ec2ab8d95afba90f16806a282c9d4abd987
kubapok <jakubpokrywka@gmail.com>
2016-12-11 13:58:49 +0100

    works, but still needs refactorization

--- serwer/models/model.py
+++ serwer/models/model.py
@@ -15,31 +15,26 @@ class Model():
     #changes "_id":{"$oid":" "5836049c57aac220c4c87384"} to simple $oid:" "5836049c57aac220c4c87384"
     #and changes list of ObjectID type, to list of strings
     #maybe that should be recursive in some way?
+
+    def prettyIdRepresentationOfItem(collection):
+        prettyCollection = collection
+        try:
+            prettyCollection['id']=str(prettyCollection['_id'])
+            del prettyCollection['_id']
+        except:
+            pass
+        for i in prettyCollection:
+            if isinstance(prettyCollection[i],list) and isinstance(prettyCollection[i][0],ObjectId):
+                prettyCollection[i] = [str(a) for a in prettyCollection[i]]
+        return prettyCollection
+
+
     def prettyIdRepresentation(collection):
         prettyCollection = collection
         print(type(collection))
         if  isinstance(prettyCollection,dict):
-            try:
-                prettyCollection['id']=str(prettyCollection['_id'])
-                del prettyCollection['_id']
-            except:
-                pass
-            for i in prettyCollection:
-                if isinstance(prettyCollection[i],list) and isinstance(prettyCollection[i][0],ObjectId):
-                    prettyCollection[i] = [str(a) for a in prettyCollection[i]]
-            return prettyCollection
-
-
-        for item in prettyCollection:
-            try:
-                item['id']=str(item['_id'])
-                del item['_id']
-            except:
-                pass
-            for i in item:
-                if isinstance(item[i],list) and isinstance(item[i][0],ObjectId):
-                    item[i] = [str(a) for a in item[i]]
-        return prettyCollection
+            return Model.prettyIdRepresentationOfItem(prettyCollection)
+        return [Model.prettyIdRepresentationOfItem(item) for item in prettyCollection]
 
     @classmethod
     def getAll(cls):
@@ -50,10 +45,6 @@ class Model():
 
     @classmethod
     def getById(cls,id):
-        print(type(cls.collection.find_one({'_id' :ObjectId(id)})))
-        print('='*30)
-        print(cls.collection.find_one({'_id' :ObjectId(id)}))
-        print('='*30)
         collection = cls.collection.find_one({'_id' :ObjectId(id)})
         collection = Model.prettyIdRepresentation(collection)
         return dumps(collection,ensure_ascii=False,indent=4, default=default).encode("utf8")
