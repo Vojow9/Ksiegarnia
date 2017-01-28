2bdae9889ec653a8ab01142c870ac387c953d888
kubapok <jakubpokrywka@gmail.com>
2017-01-28 11:16:23 +0100

    deployment option added

--- serwer/run.py
+++ serwer/run.py
@@ -4,10 +4,17 @@ import sys
 
 
 def main(port = 8080):
-    run(host='localhost', port=8080, debug=True)
+    run(host='localhost', port=port, debug=True)
+
+
+def deployment(port = 8080):
+    run(host='localhost', port=port, server ='paste', debug=True)
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         main(sys.argv[1])
+    elif len(sys.argv) == 3:
+        deployment(sys.argv[1])
     else:
         main()
